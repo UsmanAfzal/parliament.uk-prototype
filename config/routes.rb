64bcae68d27288e67b3d7edd721ea2c860d1e524
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-06-02 12:49:28 +0100

    Added routes for resource and resource/:id

--- config/routes.rb
+++ config/routes.rb
@@ -232,6 +232,14 @@ Rails.application.routes.draw do
     lookupable('houses#lookup_by_letters')
   end
 
+  ## Resource
+  # /resource/
+  scope '/resource', as: 'resource' do
+    get '/', to: 'resource#index'
+    scope '/:resource_id' do
+      get '/', to: 'resource#show', resource_id: id_format_regex
+    end
+  end
 
   ## Meta ##
   # /meta
