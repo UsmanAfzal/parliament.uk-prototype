0d218851da47c99f6b742addc69180b475d5efe1
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-06-06 09:46:23 +0100

    Add tests

--- spec/controllers/resource_controller_spec.rb
+++ spec/controllers/resource_controller_spec.rb
@@ -15,23 +15,24 @@ RSpec.describe ResourceController, vcr: true do
   describe 'GET grom nodes' do
     context 'successfully' do
       before(:each) do
-        get :show, params: { resource_id: '7TX8ySd4' }
+        get :show, params: { resource_id: 'xP2kB45W' }
       end
 
-      it 'assigns @nodes if grom nodes can be found' do
-        assigns(:nodes).each do |node|
-          expect(node).to be_a(Grom::Node)
+      it 'assigns @results if grom nodes can be found' do
+        assigns(:results).each do |result|
+          expect(result).to be_a(Grom::Node)
         end
+      end
+
+      it 'redirects to people path' do
+        expect(response).to be_redirect
       end
     end
 
     context 'unsuccessfully' do
-      before(:each) do
-        get :show, params: { resource_id: '12345678' }
-      end
 
       it 'raises error if no grom nodes can be found (404)' do
-        expect(response).not_to have_http_status(:ok)
+        expect{get :show, params: { resource_id: '12345678' }}.to raise_error(ActionController::RoutingError)
       end
     end
   end
